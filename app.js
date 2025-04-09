0d29cb49645ed5e0bd4633954e9b2a372ef3c6b7
Akshay <akshaymina47@gmail.com>
2025-04-09 15:37:04 +0530

    push the most recent commit to github

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 const flash = require('connect-flash');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const methodOverride = require('method-override');
 require('dotenv').config();
 
 // Import passport config
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 
 // View engine setup
 app.set('view engine', 'ejs');
